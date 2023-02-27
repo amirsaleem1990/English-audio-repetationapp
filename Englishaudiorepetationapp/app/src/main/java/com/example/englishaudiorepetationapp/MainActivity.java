309e49cdbfb6a0b790b7f1dae3775ae9edc27642
amirsaleem1990 <amirsaleem1990@hotmail.com>
2023-02-27 19:30:32 +0500

    Successfully reads the file last_file.txt | About to solve the writing problem

--- Englishaudiorepetationapp/app/src/main/java/com/example/englishaudiorepetationapp/MainActivity.java
+++ Englishaudiorepetationapp/app/src/main/java/com/example/englishaudiorepetationapp/MainActivity.java
@@ -1,5 +1,16 @@
 package com.example.englishaudiorepetationapp;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import android.Manifest;
 import android.content.ContentResolver;
 import android.content.ContentValues;
@@ -30,7 +41,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String LAST_FILE_NAME = Environment.getExternalStorageDirectory().getAbsolutePath() + "/last_file.txt";
+//    private static final String LAST_FILE_NAME = Environment.getExternalStorageDirectory().getAbsolutePath() + "/last_file.txt";
 
     private Button repeatButton;
     private Button nextButton;
@@ -38,8 +49,13 @@ public class MainActivity extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
 
     private int currentNumber = 1;
+//    private JSONObject jsonObject;
 
     private static final int STORAGE_PERMISSION_REQUEST_CODE = 1;
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
+
+    private static final int PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
+
 
 
     @Override
@@ -48,8 +64,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_REQUEST_CODE);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE);
 
-//        Toast.makeText(this, "AMIR", Toast.LENGTH_SHORT).show();
 
         // Initialize the buttons and text box
         repeatButton = findViewById(R.id.repeat_button);
@@ -58,6 +75,33 @@ public class MainActivity extends AppCompatActivity {
 
         // First, create the file object for the file you want to save
         File text_file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "last_file.txt");
+        File json_file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "mapping.json");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(json_file))) {
+            // read the contents of the file into a StringBuilder
+            StringBuilder jsonBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonBuilder.append(line);
+            }
+            // create a JSONTokener from the contents of the file
+            JSONTokener tokener = new JSONTokener(jsonBuilder.toString());
+            // parse the JSONTokener into a JSONObject
+//            JSONObject x = new JSONObject(tokener);
+//            jsonObject = x;
+            JSONObject jsonObject = new JSONObject(tokener);
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+            Log.d("MainActivity", "---------------- Error in json: " + e);
+        }
+
+        // read values from the JSONObject
+        String text_to_display = jsonObject.getString("3");
+        textBox.setText(text_to_display);
+
+
+
+        Log.d("MainActivity", "----------------: " +  text_file);
 
         // Next, create the ContentValues object with the metadata for the file
         ContentValues values = new ContentValues();
@@ -69,21 +113,25 @@ public class MainActivity extends AppCompatActivity {
         ContentResolver resolver = getContentResolver();
         Uri uri = resolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, values);
 
-        // Read the current number from the text file
-//        int x = read_from_text_file(text_file, uri, resolver);
-        int x = 1;
-        currentNumber = x;
 
+
+
+
+        // Read the current number from the text file
         if (text_file.exists()) {
             Log.d("MainActivity", "---------------- Text File is exists");
             try {
-                FileInputStream inputStream = new FileInputStream(String.valueOf(uri));
-                byte[] buffer = new byte[1024];
-                int read = inputStream.read(buffer);
-                String contents = new String(buffer, 0, read);
-                int currentNumber = Integer.parseInt(contents);
-                Log.d("MainActivity", "---------------- currentNumber: " + currentNumber);
-                inputStream.close();
+                byte[] bytes = Files.readAllBytes(Paths.get(String.valueOf(text_file)));
+                String x = new String(bytes);
+                try{
+                    String numericOnly = x.replaceAll("[^\\d]", "");
+                    int num = Integer.parseInt(numericOnly);
+                    currentNumber = num;
+                } catch (Exception e){
+                    Log.d("MainActivity", "---------------- x: " + x);
+                    Log.d("MainActivity", "---------------- >>>>>>>> Error: " + e);
+                }
+                Log.d("MainActivity", "----------------currentNumber read from file: " + currentNumber);
             } catch (IOException e) {
                 Log.d("MainActivity", "---------------- Failed to read the text file: " + e);
                 e.printStackTrace();
@@ -101,8 +149,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-
-
         repeatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -113,14 +159,18 @@ public class MainActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("MainActivity", "---------------- Clicked on 'nextButton', the current currentNumber is: " + currentNumber);
                 currentNumber++;
                 playAudioFile(currentNumber);
-
                 // Save the current number to last_file.txt
+                File text_file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "last_file.txt");
                 try {
-                    FileOutputStream outputStream = new FileOutputStream(text_file);
-                    outputStream.write(String.valueOf(currentNumber).getBytes());
-                    outputStream.close();
+                    Log.d("MainActivity", "---------------- text_file: " + text_file);
+                    FileWriter writer = new FileWriter(text_file);
+//                    String currentNumber_str = Integer.toString(currentNumber);
+//                    writer.write(currentNumber_str);
+                    writer.write("108");
+                    writer.close();
                     Log.d("MainActivity", "---------------- Succussfully wrote the currentNumber (" + currentNumber + ") to the text file");
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -129,7 +179,7 @@ public class MainActivity extends AppCompatActivity {
                     } else {
                         Log.d("MainActivity", "---------------- The text file is NOT exists");
                     }
-                    Log.d("MainActivity", "---------------- Failed to write the currentNumber (" + currentNumber + ") to the text file" + e);
+                    Log.d("MainActivity", "---------------- Failed to write the currentNumber (" + currentNumber + ") to the text file | " + e);
                 }
 
             }
@@ -146,6 +196,7 @@ public class MainActivity extends AppCompatActivity {
         if (file.exists()) {
             Log.d("MainActivity", "---------------- File path: " + filePath + "is Exists");
         }
+        Log.d("MainActivity", "---------------- 'playAudioFile' is called, the current currentNumber is: " + currentNumber);
 
         mediaPlayer = new MediaPlayer();
         try {
